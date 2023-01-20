6d1bed6d4b1a87901baea75a529690699afd1550
Ulises <ulivera01@gmail.com>
2023-01-19 23:37:38 -0300

    Toques

--- codigo.js
+++ codigo.js
@@ -1,6 +1,6 @@
 class Producto{
-    constructor(tipoEquipo, delivery, precio){
-    this.tipoEquipo = tipoEquipo;
+    constructor(tipoAlimento, delivery, precio){
+    this.tipoAlimento = tipoAlimento;
     this.delivery = delivery;
     this.precio = parseFloat(precio); 
     }
@@ -35,29 +35,29 @@ while (comprar != "no") {
     let product = new Producto(productName, "", precios[productName])
     let deliveryType = prompt("Ingrese el tipo de envio(para hoy o normal): ")
     product.delivery = deliveryType
-    if (product.tipoEquipo == "pollo" && product.delivery == "hoy") {
-      console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    if (product.tipoAlimento == "pollo" && product.delivery == "hoy") {
+      console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
       + product.getPrecioConEnvio())
-    }if (product.tipoEquipo == "pollo" && product.delivery == "normal") {
-        console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    }if (product.tipoAlimento == "pollo" && product.delivery == "normal") {
+        console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
         + product.getPrecioConEnvio())
-    }if (product.tipoEquipo == "huevos" && product.delivery == "hoy") {
-        console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    }if (product.tipoAlimento == "huevos" && product.delivery == "hoy") {
+        console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
         + product.getPrecioConEnvio())
-    }if (product.tipoEquipo == "huevos" && product.delivery == "normal") {
-        console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    }if (product.tipoAlimento == "huevos" && product.delivery == "normal") {
+        console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
         + product.getPrecioConEnvio())
-    }if (product.tipoEquipo == "milanesas" && product.delivery == "hoy") {
-        console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    }if (product.tipoAlimento == "milanesas" && product.delivery == "hoy") {
+        console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
         + product.getPrecioConEnvio())
-    }if (product.tipoEquipo == "milanesas" && product.delivery == "normal") {
-        console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    }if (product.tipoAlimento == "milanesas" && product.delivery == "normal") {
+        console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
         + product.getPrecioConEnvio())
-    }if (product.tipoEquipo == "pelotas" && product.delivery == "hoy") {
-        console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    }if (product.tipoAlimento == "pelotas" && product.delivery == "hoy") {
+        console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
         + product.getPrecioConEnvio())
-    }if (product.tipoEquipo == "pelotas" && product.delivery == "normal") {
-        console.log("Ha agregado al carrito " + product.tipoEquipo + " por un precio de: " + product.precio + " quedando con el envio: "
+    }if (product.tipoAlimento == "pelotas" && product.delivery == "normal") {
+        console.log("Ha agregado al carrito " + product.tipoAlimento + " por un precio de: " + product.precio + " quedando con el envio: "
         + product.getPrecioConEnvio())
     }
         
